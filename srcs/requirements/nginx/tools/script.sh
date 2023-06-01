5576317f1122437e0e4eea59ab94a2e0c7d538b5
MAR BEN <marouanebenbajja@gmail.com>
2023-06-01 05:31:42 +0100

    . Nginx Service :     + SSL generated and used.     + Exposing port 443.
        - Forbidden or bad request problem.
        - fastcgi wordpress service.

---
+++ srcs/requirements/nginx/tools/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MO/L=MA/O=1337/OU=student/CN=mbenbajj.1337.ma"
+
+nginx -g "daemon off;"
