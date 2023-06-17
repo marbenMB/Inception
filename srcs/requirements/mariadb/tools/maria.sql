fe4d0c43788daeb71ffd815dc67c7d1239557a3c
marbenMB <mbenbajj@student.1337.ma>
2023-06-17 15:44:51 +0100

    MariaDB service : 	+ Done.

--- srcs/requirements/mariadb/tools/maria.sql
+++
@@ -1,8 +0,0 @@
-SET @dbname = '$M_DB_NAME';
-CREATE DATABASE IF NOT EXISTS '@dbname';
-
-SET @user = '$M_USER_NAME';
-SET @pass = '$M_USER_PASS';
-CREATE USER '@user'@'%' IDENTIFIED BY '@pass';
-GRANT ALL PRIVILIGES ON '@dbname'.* TO '@user'@'%';
-FLUSH PRIVILIGES;
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,7 +2,14 @@
 
 service mariadb start
 
-mariadb < /var/maria.sql
+echo "CREATE DATABASE ${M_DB_NAME};" > maria.sql
+echo "CREATE USER '${M_USER_NAME}'@'%' IDENTIFIED BY '${M_USER_PASS}';" >> maria.sql
+echo "GRANT ALL PRIVILEGES ON ${M_DB_NAME}.* TO '${M_USER_NAME}'@'%';" >> maria.sql
+echo "FLUSH PRIVILEGES;" >> maria.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${M_ROOT_PASS}';" >> maria.sql
 
-# mysqld_safe 
-tail -f;
+mariadb < maria.sql
+
+mysqladmin shutdown -p${M_ROOT_PASS} 
+
+exec "$@"
