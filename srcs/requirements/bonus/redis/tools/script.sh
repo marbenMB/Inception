faf3581da05dd2d53c9abcba7233258c0b630414
marbenMB <mbenbajj@student.1337.ma>
2023-06-24 14:47:08 +0100

    Redis Optim

--- srcs/requirements/bonus/redis/tools/script.sh
+++ srcs/requirements/bonus/redis/tools/script.sh
@@ -4,6 +4,6 @@ echo "maxmemory 256mb" >> /etc/redis/redis.conf
 
 echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
 
-sed -i -r "s|bind 127.0.0.1 ::1|bind wordpress ::1|g" /etc/redis/redis.conf
+sed -i -r "s|bind 127.0.0.1 ::1|bind 0.0.0.0 ::1|g" /etc/redis/redis.conf
 
 exec "$@"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -14,26 +14,24 @@ cat /var/www/html/wp-config-sample.php > /var/www/html/wp-config.php
 
 cd /var/www/html/
 
-wp config set DB_NAME $M_DB_NAME --path=/var/www/html/ --allow-root
-wp config set DB_USER $M_USER_NAME --path=/var/www/html/ --allow-root
-wp config set DB_PASSWORD $M_USER_PASS --path=/var/www/html/ --allow-root
-wp config set DB_HOST $W_DB_HOST --path=/var/www/html/ --allow-root
+wp config set DB_NAME $M_DB_NAME  --allow-root
+wp config set DB_USER $M_USER_NAME  --allow-root
+wp config set DB_PASSWORD $M_USER_PASS  --allow-root
+wp config set DB_HOST $W_DB_HOST  --allow-root
 
-wp config set WP_REDIS_HOST "redis" --path=/var/www/html/  --allow-root 
-wp config set WP_REDIS_PORT "6379"  --path=/var/www/html/  --allow-root 
+wp config set WP_REDIS_HOST "$REDIS_HOST" --allow-root
+wp config set WP_REDIS_PORT "$REDIS_PORT" --allow-root
 
-wp core install --url=$DOMAINE_NAME --title=$W_TITLE --admin_user=$W_ADMIN_U --admin_password=$W_ADMIN_P --admin_email=$W_ADMIN_E --path=/var/www/html --skip-email --allow-root
+wp core install --url=$DOMAINE_NAME --title=$W_TITLE --admin_user=$W_ADMIN_U --admin_password=$W_ADMIN_P --admin_email=$W_ADMIN_E --skip-email --allow-root
 
 wp user create $USER_N $EMAIL --role=$ROLE --user_pass=$USER_P --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
-wp theme install Neve --activate --path=/var/www/html/ --allow-root
+wp plugin install redis-cache --activate  --allow-root
 
-wp plugin install redis-cache --activate --path=/var/www/html/ --allow-root
+wp plugin update --all  --allow-root
 
-wp plugin update --all --path=/var/www/html/ --allow-root
-
-wp redis enable --path=/var/www/html/ --allow-root
+wp redis enable  --allow-root
 
 exec "$@"
